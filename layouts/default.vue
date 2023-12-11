3b8b56fcb9f86bba1a4cf98d87929674c1b85512
dev-misha <romanov.players@gmail.com>
2023-12-11 19:17:42 +0300

    fix(settimeout autoplay)

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,7 @@
     <Preloader :critical="true"/>
     <div class="main-mask__wrap">
       <div class="main-mask --mob">
-        <video muted autoplay loop playsinline webkit-playsinline controls="false">
+        <video class="video-bg" muted loop playsinline webkit-playsinline controls="false">
           <source src="/mbbg.mp4" />
         </video>
       </div>
@@ -23,6 +23,11 @@
 export default {
   // GSAPScrollSmoother,
   mounted() {
+
+    const video = document.getElementsByClassName('video-bg');
+    setTimeout(()=> {
+      video.play()
+    }, 3000)
     window.addEventListener('resize', () => {
       let vh = window.innerHeight * 0.01;
       document.documentElement.style.setProperty('--vh', `${vh}px`);
